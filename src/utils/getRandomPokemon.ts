ad98d83848743fa8e496462f213088739ff49a43
kcselm <kcselm93@gmail.com>
2023-01-13 17:38:45 -0800

    Add random pokemon id fetcher

---
+++ src/utils/getRandomPokemon.ts
@@ -0,0 +1,14 @@
+const MAX_POKEDEX_NUMBER = 150
+
+export const getRandomPokemon: (notThisOne?: number) => number = notThisOne => {
+  const pokedexNumber = Math.floor(Math.random() * MAX_POKEDEX_NUMBER) + 1
+
+  if (pokedexNumber !== notThisOne) return pokedexNumber
+  return getRandomPokemon(notThisOne)
+}
+
+export const getOptionsForVote = () => {
+  const firstId = getRandomPokemon()
+  const secondId = getRandomPokemon(firstId)
+  return [firstId, secondId]
+}
